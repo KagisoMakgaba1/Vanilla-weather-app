edd877b0c47ba3bc59c56adf358274a8d64b9ce4
kagisomakgaba <kagisomakgaba99@gmail.com>
2023-09-03 05:34:10 +0200

    Updated javascript

--- src/index.js
+++ src/index.js
@@ -1,22 +1,49 @@
+function currentDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
-  console.log(response.data);
   let city = document.querySelector("#city");
-  city.innerHTML = response.data.name;
-
   let temperature = document.querySelector("#temperature");
-  temperature.innerHTML = Math.round(response.data.main.temp);
-
   let description = document.querySelector("#description");
-  description.innerHTML = response.data.weather[0].description;
-
   let humidity = document.querySelector("#humidity");
-  humidity.innerHTML = response.data.main.humidity;
-
   let wind = document.querySelector("#wind");
+  let date = document.querySelector("#date");
+
+  city.innerHTML = response.data.name;
+  temperature.innerHTML = Math.round(response.data.main.temp);
+  description.innerHTML = response.data.weather[0].description;
+  humidity.innerHTML = response.data.main.humidity;
   wind.innerHTML = Math.round(response.data.wind.speed);
+  date.innerHTML = currentDate(response.data.dt * 1000);
 }
 
 let apiKey = "f606a147394d27601839ffd5343624ee";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Johannesburg&appid=${apiKey}&units=metric`;
+let city = "Johannesburg";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
